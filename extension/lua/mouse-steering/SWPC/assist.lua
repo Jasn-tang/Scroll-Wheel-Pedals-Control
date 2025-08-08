41c54e61bef10812001a4a17167758aefcb8d9f1
Jason Tang <jasontang0724@gmail.com>
2025-08-08 18:59:33 +0800

    Add files via upload

--- extension/lua/mouse-steering/SWPC/assist.lua
+++ extension/lua/mouse-steering/SWPC/assist.lua
@@ -11,37 +11,28 @@ local steeringFinal = 0
 
 function script.update(dt, deltaX)
     --Mouse Steering--
-    steeringFinal = steeringFinal + deltaX
-    steeringFinal = math.clamp(steeringFinal, -1, 1)
+    steeringFinal = math.clamp(steeringFinal + deltaX, -1, 1)
     
     --Throttle Part--
-    if ac.isKeyDown(ac.KeyIndex[settings.throttle]) then
-        if ac.isKeyPressed(ac.KeyIndex[settings.throttle]) then gasFinal = 0.25
-        elseif ac.getUI().mouseWheel > 0 then
-            gasFinal = math.min(gasFinal + settings.scroll, 1.0)
-        elseif ac.getUI().mouseWheel < 0 then
-            gasFinal = math.max(gasFinal - settings.scroll, 0.0)
-        end
-    elseif ac.isKeyReleased(ac.KeyIndex[settings.throttle]) then gasFinal = 0.0
-    end
-
     if ac.isKeyDown(ac.KeyIndex[settings.throttle]) and ac.isKeyDown(ac.KeyIndex.Shift) then
-        gasFinal = 1.0
+        gasFinal = 1
+    elseif ac.isKeyPressed(ac.KeyIndex[settings.throttle]) then gasFinal = 0.25
+    elseif ac.isKeyDown(ac.KeyIndex[settings.throttle]) then
+        if ac.getUI().mouseWheel ~= 0 then
+            gasFinal = math.clamp(gasFinal + ac.getUI().mouseWheel * settings.scroll, 0, 1)
+        end
+    else gasFinal = 0
     end
 
     --Brake Part--
-    if ac.isKeyDown(ac.KeyIndex[settings.brake]) then
-        if ac.isKeyPressed(ac.KeyIndex[settings.brake]) then brakeFinal = 0.25
-        elseif ac.getUI().mouseWheel > 0 then
-            brakeFinal = math.min(brakeFinal + settings.scroll, 1.0)
-        elseif ac.getUI().mouseWheel < 0 then
-            brakeFinal = math.max(brakeFinal - settings.scroll, 0.0)
-        end
-    elseif ac.isKeyReleased(ac.KeyIndex[settings.brake]) then brakeFinal = 0.0
-    end
-
     if ac.isKeyDown(ac.KeyIndex[settings.brake]) and ac.isKeyDown(ac.KeyIndex.Shift) then
-        brakeFinal = 1.0
+        brakeFinal = 1
+    elseif ac.isKeyPressed(ac.KeyIndex[settings.brake]) then brakeFinal = 0.25
+    elseif ac.isKeyDown(ac.KeyIndex[settings.brake]) then
+        if ac.getUI().mouseWheel ~= 0 then
+            brakeFinal = math.clamp(brakeFinal + ac.getUI().mouseWheel * settings.scroll, 0, 1)
+        end
+    else brakeFinal = 0
     end
 
     --Output Part-
